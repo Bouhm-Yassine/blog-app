3b8be1114dc6c21a041f5e75cf794c369e5f9a99
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-24 17:03:05 +0100

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
- 
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
 function App() {
-  const title = 'BLOG Application'
-  const number = 167
-  const sick = true
-  const person = {"name": "yassine"}
-  const url = 'http://www.google.com'
   return (
     <div className="App">
-    <div className="content">
-      <h1>Person: {person.name}</h1>
-      <h2>REACT - {title}</h2>
-      <h5>Number of components - {number}</h5>
-      <h5>State - {sick ? 'Healthy': 'Sick'}</h5>
-      <h5>
-        <a href={url} target="_blank" rel='noreferrer'>Click Here</a>
-      </h5>
-    </div>
+      <Navbar></Navbar>
+      
+      <div className="content">
+        <Home></Home>
+      </div>
+
+
     </div>
   );
 }

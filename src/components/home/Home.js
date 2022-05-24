3a2e1258a919ab55b7703d6af3fd33fa39b0c83f
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-24 18:14:18 +0100

    use state hook

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,22 @@
 import './Home.css'
-
-const handleClick = (event) => {
-    console.log("CLICKED", event);
-}
-
-const handleClickParam = (event, name) => {
-    console.log("HELLO", name, event);
-}
+import { useState } from 'react'
 
 const Home = () => {
+    const [name, setName] = useState('Yassine');
+
+    const handleClick = (event) => {console.log("CLICKED", event);}
+    const handleClickParam = (event, name) => {console.log("HELLO", name, event);}
+
+
     return (
         <div className="content">
             <h2>Home Component</h2>
+            <p>{name}</p>
+            <div className="btn-container">
             <button onClick={handleClick}>Click</button>
             <button onClick={(event) => handleClickParam(event, 'YASSINE')}>Click Param</button>
+            <button onClick={() => setName('Name Chnaged')}>Click to change name</button>
+            </div>
         </div>
     );
 }

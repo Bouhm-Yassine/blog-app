9eb177d1501dc68d3542e8b6a1b30447e95d0c30
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-24 17:35:57 +0100

    Inline styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,7 +6,8 @@ const Navbar = () => {
             <h1>Bom's Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">Home</a>
+                <a href="/create" style={{ color: 'white', backgroundColor: "#f1356d", borderRadius: "5px"}}>
+                    New blog</a>
             </div>
         </nav>
     );

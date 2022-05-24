f2a6870bde59cb66311525c737a8f9f73465e697
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-24 17:56:41 +0100

    Click events

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,19 @@
 import './Home.css'
 
+const handleClick = (event) => {
+    console.log("CLICKED", event);
+}
+
+const handleClickParam = (event, name) => {
+    console.log("HELLO", name, event);
+}
+
 const Home = () => {
     return (
         <div className="content">
             <h2>Home Component</h2>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={(event) => handleClickParam(event, 'YASSINE')}>Click Param</button>
         </div>
     );
 }

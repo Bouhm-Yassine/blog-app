e41dae8e21bcef8849dc547228eeb07bda2ea633
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-24 18:30:23 +0100

    clean up

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,21 +2,10 @@ import './Home.css'
 import { useState } from 'react'
 
 const Home = () => {
-    const [name, setName] = useState('Yassine');
-
-    const handleClick = (event) => {console.log("CLICKED", event);}
-    const handleClickParam = (event, name) => {console.log("HELLO", name, event);}
-
-
+    
     return (
         <div className="content">
             <h2>Home Component</h2>
-            <p>{name}</p>
-            <div className="btn-container">
-            <button onClick={handleClick}>Click</button>
-            <button onClick={(event) => handleClickParam(event, 'YASSINE')}>Click Param</button>
-            <button onClick={() => setName('Name Chnaged')}>Click to change name</button>
-            </div>
         </div>
     );
 }

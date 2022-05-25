a53eaf70fd3aae52ef72f89c2c819a2355653472
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-25 21:18:48 +0100

    Resuse component with different data

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,19 +3,21 @@ import { useState } from 'react'
 import BlogList from '../blog-list/BlogList';
 
 const Home = () => {
-    const [title, setTitle] = useState('All Blogs !')
+    // const [title, setTitle] = useState('All Blogs !')
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
-
-
+    
+    const getBlogsByAuthor = (author) => {
+        return blogs.filter((item) => item.author === author)
+    }
+ 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={title}></BlogList>
-
-            <button onClick={() => setTitle('All Users !')}>Click Me</button>
+            <BlogList blogs={blogs} title="All Blogs !"></BlogList>
+            <BlogList blogs={getBlogsByAuthor('mario')} title="Mario's Blogs !"></BlogList>
         </div>
     );
 }
